0bda20baca4396e653be923e334e72bde71b3fe6
Tim Schindler <xh3b4sd@gmail.com>
2020-11-09 22:30:52 +0100

    add workflows (#1)

--- src/pbf/post/search_pb.d.ts
+++ src/pbf/post/search_pb.d.ts
@@ -1,6 +1,4 @@
-import * as jspb from 'google-protobuf'
-
-
+import * as jspb from "google-protobuf";
 
 export class SearchI extends jspb.Message {
   getName(): string;
@@ -9,15 +7,21 @@ export class SearchI extends jspb.Message {
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): SearchI.AsObject;
   static toObject(includeInstance: boolean, msg: SearchI): SearchI.AsObject;
-  static serializeBinaryToWriter(message: SearchI, writer: jspb.BinaryWriter): void;
+  static serializeBinaryToWriter(
+    message: SearchI,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): SearchI;
-  static deserializeBinaryFromReader(message: SearchI, reader: jspb.BinaryReader): SearchI;
+  static deserializeBinaryFromReader(
+    message: SearchI,
+    reader: jspb.BinaryReader
+  ): SearchI;
 }
 
 export namespace SearchI {
   export type AsObject = {
-    name: string,
-  }
+    name: string;
+  };
 }
 
 export class SearchO extends jspb.Message {
@@ -27,14 +31,19 @@ export class SearchO extends jspb.Message {
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): SearchO.AsObject;
   static toObject(includeInstance: boolean, msg: SearchO): SearchO.AsObject;
-  static serializeBinaryToWriter(message: SearchO, writer: jspb.BinaryWriter): void;
+  static serializeBinaryToWriter(
+    message: SearchO,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): SearchO;
-  static deserializeBinaryFromReader(message: SearchO, reader: jspb.BinaryReader): SearchO;
+  static deserializeBinaryFromReader(
+    message: SearchO,
+    reader: jspb.BinaryReader
+  ): SearchO;
 }
 
 export namespace SearchO {
   export type AsObject = {
-    message: string,
-  }
+    message: string;
+  };
 }
-
